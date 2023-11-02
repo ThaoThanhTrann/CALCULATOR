6b8675b13f9495d0b902655289e2f0f2478018bc
thanh thao <tranthithanhthao10012005@gmail.com>
2023-11-02 16:42:43 +0700

    Update Form1

--- CALCULATOR/Form1.Designer.cs
+++ CALCULATOR/Form1.Designer.cs
@@ -28,12 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btCong = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtKetqua = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btCong
+            // 
+            this.btCong.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btCong.ForeColor = System.Drawing.Color.Blue;
+            this.btCong.Location = new System.Drawing.Point(584, 363);
+            this.btCong.Name = "btCong";
+            this.btCong.Size = new System.Drawing.Size(94, 60);
+            this.btCong.TabIndex = 0;
+            this.btCong.Text = "+";
+            this.btCong.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(90, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Số a:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(227, 155);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(451, 27);
+            this.txtA.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(800, 100);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "TÍNH TOÁN";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(90, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 41);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Số b:";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(227, 223);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(451, 27);
+            this.txtB.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(90, 278);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 41);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Kết quả:";
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtKetqua
+            // 
+            this.txtKetqua.Location = new System.Drawing.Point(227, 292);
+            this.txtKetqua.Name = "txtKetqua";
+            this.txtKetqua.Size = new System.Drawing.Size(451, 27);
+            this.txtKetqua.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.txtKetqua);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btCong);
+            this.Name = "Form1";
+            this.Text = "Tinh toan";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button btCong;
+        private Label label1;
+        private TextBox txtA;
+        private Label label2;
+        private Label label3;
+        private TextBox txtB;
+        private Label label4;
+        private TextBox txtKetqua;
     }
 }
